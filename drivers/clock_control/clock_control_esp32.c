72b7e3398d69577e960b55713a6e205d397a54eb
Bui Trung Kien <trungkiena6@gmail.com>
2024-06-18 21:53:17 +0700

    clock_control_esp32.c: Added to initialize wifi clock

--- drivers/clock_control/clock_control_esp32.c
+++ drivers/clock_control/clock_control_esp32.c
@@ -78,8 +78,19 @@ static bool reset_reason_is_cpu_reset(void)
 }
 
 #if defined(CONFIG_SOC_SERIES_ESP32C6)
+#include "modem_clock_types.h"
+
 static void esp32_clock_perip_init(void)
 {
+	soc_rtc_slow_clk_src_t rtc_slow_clk_src = rtc_clk_slow_src_get();
+    modem_clock_lpclk_src_t modem_lpclk_src = (modem_clock_lpclk_src_t) ( \
+                  (rtc_slow_clk_src == SOC_RTC_SLOW_CLK_SRC_RC_SLOW)  ? MODEM_CLOCK_LPCLK_SRC_RC_SLOW \
+                : (rtc_slow_clk_src == SOC_RTC_SLOW_CLK_SRC_XTAL32K)  ? MODEM_CLOCK_LPCLK_SRC_XTAL32K  \
+                : (rtc_slow_clk_src == SOC_RTC_SLOW_CLK_SRC_RC32K)    ? MODEM_CLOCK_LPCLK_SRC_RC32K    \
+                : (rtc_slow_clk_src == SOC_RTC_SLOW_CLK_SRC_OSC_SLOW) ? MODEM_CLOCK_LPCLK_SRC_EXT32K   \
+                : SOC_RTC_SLOW_CLK_SRC_RC_SLOW);
+    modem_clock_select_lp_clock_source(PERIPH_WIFI_MODULE, modem_lpclk_src, 0);
+
 	soc_reset_reason_t rst_reason = esp_rom_get_reset_reason(0);
 
 	if ((rst_reason != RESET_REASON_CPU0_MWDT0) && (rst_reason != RESET_REASON_CPU0_MWDT1) &&
